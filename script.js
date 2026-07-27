9147cdd9a4fd9caf94e9d29f0665ea7853445de5
rizvimustafa1 <rizvimustafa1@gmail.com>
2026-07-27 15:12:12 +0100

    Add functions to show message and handle popup

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function showMessage(){
 
+document.getElementById("hiddenMessage").style.display="block";
+
+}
+
+function love(){
+
+document.getElementById("popup").style.display="flex";
+
+}
+
+function closePopup(){
+
+document.getElementById("popup").style.display="none";
+
+}
